94fc38ec7c01d68390b8c8a66050e9bbc173e351
vladopard <vladislavvidovic22@gmail.com>
2024-06-21 02:09:22 +0200

    dodate rute

--- front/src/App.js
+++ front/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './Pocetna/HomePage';
@@ -6,12 +8,15 @@ import HistoricalEvents from './ninjasApi/HistoricalEvents';
 
 function App() {
   return (
-    <div className="App">
-      <HistoricalEvents></HistoricalEvents>
-      <AuthPage></AuthPage>
-        <HomePage></HomePage>
-       
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/historical-events" element={<HistoricalEvents />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
